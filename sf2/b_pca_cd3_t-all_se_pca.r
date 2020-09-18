1f9d7c71877ddd269f0c9cf7cff6291553f1f536
Yalu Zhou <yzh5179@northwestern.edu>
2020-09-18 16:14:18 -0500

    update

---
+++ sf2/b_pca_cd3_t-all_se_pca.r
@@ -0,0 +1,26 @@
+## PCA analysis of skipped excon events for CD3 T cells and T-ALL patients. 
+
+# Load packages
+>library(ggplot2)
+
+# Read data
+>seFile <- "CD3_T-ALL_SE_PCA.txt"
+>sepca <- as.matrix(read.table(seFile, header=TRUE, sep="\t", as.is=TRUE))
+
+# PCA analysis
+>pca <- prcomp(t(sepca), scale = TRUE)
+>pca.var <- pca$sdev^2
+>pca.var.per <- round(pca.var/sum(pca.var)*100,1)
+
+# Generate PCA plot
+pca.data <- data.frame(Samples = rownames(pca$x), 
++ X=pca$x[,1],
++ Y=pca$x[,2])
+
+ggplot(data=pca.data, aes(x=X, y=Y, label=Sample))+
++  geom_text() +
++  xlab(paste("PC1 - ", pca.var.per[1], "%", sep=""))+
++  ylab(paste("PC2 - ", pca.var.per[2], "%", sep=""))+
++  theme_bw() + 
++  ggtitle("CD3 T-cells vs T-ALL SE PCA analysis")
+
